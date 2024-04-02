b615728b4a282ad83a7e6fe260f6cb1f1181b6cd
Rahelrzele <rahelrzele@gmail.com>
2024-04-02 10:00:18 +0300

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav"  "main"`,
+        lg: `"nav nav " "asid main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"purple"}>
+        nav
+      </GridItem>
+
+      <GridItem area={"main"} bg={"pink"}>
+        main
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bg={"purple"}>
+          asid
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
 export default App;
